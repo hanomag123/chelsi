7dfb0a9ef170960329f9619676dd44a08b8844c3
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2023-03-11 14:38:18 +0300

    main

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  const xl = matchMedia('(max-width: 1024px)')
+
   class Menu {
-    constructor(menuElement, buttonElement) {
-      this.menu = typeof menuElement === "string" ? document.querySelector(menuElement) : menuElement
-      this.button = typeof buttonElement === "string" ? document.querySelector(buttonElement) : buttonElement
-      this.overlay = document.createElement('div')
-      this.overlay.hidden = true
+    constructor(menuElement, buttonElement, logo) {
+      this.menu = typeof menuElement === "string" ? document.querySelector(menuElement) : menuElement;
+      this.button = typeof buttonElement === "string" ? document.querySelector(buttonElement) : buttonElement;
+      this.overlay = document.createElement('div');
+      this.overlay.hidden = true;
+      this.buttons = this.menu.querySelectorAll('a')
+      this.logo = logo
       this._init()
     }
 
@@ -13,6 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
       document.body.appendChild(this.overlay)
       this.overlay.classList.add('overlay')
 
+      if (this.buttons.length) {
+        this.buttons.forEach(el => {
+          el.addEventListener('click', this.closeMenu.bind(this))
+        })
+      }
+      if (this.logo) {
+        this.logo.addEventListener('click', this.closeMenu.bind(this))
+      }
       this.overlay.addEventListener('click', this.toggleMenu.bind(this))
       this.button.addEventListener('click', this.toggleMenu.bind(this))
     }
@@ -30,8 +42,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     closeMenu() {
-      this.menu.classList.remove('header__nav--active')
-      this.button.classList.remove('header__menu-button--active')
+      this.menu.classList.remove('menu--open')
+      this.button.classList.remove('menu-button--active')
       this.overlay.hidden = true
 
       this.enableScroll()
@@ -63,6 +75,117 @@ document.addEventListener("DOMContentLoaded", () => {
   if (menu && menuButton) {
     new Menu(menu, menuButton)
   }
+
+  const header = document.querySelector('header')
+
+  let handler;
+
+  function scrollAdd() {
+    /* ... */
+    handler = throttle(function (event) {
+      scrollHeader();
+    }, 500);
+    document.addEventListener('scroll', handler, false);
+  }
+
+  scrollAdd()
+
+  var prevScrollpos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+  function scrollHeader() {
+    var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScrollPos < 0) {
+      currentScrollPos = 0
+      prevScrollpos = 0
+    }
+    if (prevScrollpos < 0) {
+      prevScrollpos = 0
+      currentScrollPos = 0
+    }
+    const num = xl.matches ? 50 : 150
+    if (currentScrollPos > num) {
+      header.classList.add('header--active')
+    } else {
+      header.classList.remove('header--active')
+    }
+    if (prevScrollpos >= currentScrollPos) {
+      header.classList.remove('out')
+    } else {
+      header.classList.add('out')
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
+  function throttle(func, ms) {
+    let isThrottled = false,
+      savedArgs,
+      savedThis;
+
+    function wrapper() {
+
+      if (isThrottled) { // (2)
+        savedArgs = arguments;
+        savedThis = this;
+        return;
+      }
+
+      func.apply(this, arguments); // (1)
+
+      isThrottled = true;
+
+      setTimeout(function () {
+        isThrottled = false; // (3)
+        if (savedArgs) {
+          wrapper.apply(savedThis, savedArgs);
+          savedArgs = savedThis = null;
+        }
+      }, ms);
+    }
+
+    return wrapper;
+  }
+
+
+  const numberOfSlides = document.querySelectorAll('.grid-block__swiper .swiper-slide').length
+
+  function updSwiperNumericPagination() {
+    const index = this.realIndex + 1
+    this.el.querySelector(".swiper-counter").innerHTML = '<span class="count">' + (index < 10 ? '0' + index : index) + '</span>/<span class="total">/' + (numberOfSlides < 10 ? '0' + numberOfSlides : numberOfSlides) + "</span>";
+  }
+
+  const swiper = new Swiper('.grid-block__swiper', {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.grid-block__swiper .next',
+      prevEl: '.grid-block__swiper .prev',
+    },
+    on: {
+      init: updSwiperNumericPagination,
+      slideChange: updSwiperNumericPagination
+    }
+  });
+
+    const swiper2 = new Swiper('.magazine-swiper', {
+    loop: true,
+    effect: "creative",
+    speed: 400,
+    creativeEffect: {
+      prev: {
+        translate: [0, 0, -400],
+        opacity: 0,
+      },
+      next: {
+        translate: ["100%", 0, 0],
+        opacity: 0,
+      },
+    },
+    navigation: {
+      nextEl: '.magazine-swiper .next',
+      prevEl: '.magazine-swiper .prev',
+    },
+});
+
+
 })
 
 
